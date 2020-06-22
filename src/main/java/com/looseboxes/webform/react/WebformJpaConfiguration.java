5cb8c11273f7d9e44bdd3ae71252322011471137
poshjosh <posh.bc@gmail.com>
2020-06-22 16:00:36 +0100

    Make JpaObjectFactory implementation static inner class for traceability

--- src/main/java/com/looseboxes/webform/react/WebformJpaConfiguration.java
+++ src/main/java/com/looseboxes/webform/react/WebformJpaConfiguration.java
@@ -2,6 +2,7 @@ package com.looseboxes.webform.react;
 
 import com.bc.jpa.dao.JpaObjectFactory;
 import com.bc.jpa.dao.JpaObjectFactoryBase;
+import com.bc.jpa.dao.sql.SQLDateTimePatterns;
 import com.looseboxes.webform.JpaConfiguration;
 import javax.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,25 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class WebformJpaConfiguration extends JpaConfiguration{
     
+    // Exception occured when Hibernate calls JpaObjectFactory.close during
+    // startup. The JpaObjectFactory.close method, closes the EntityManagerFactory 
+    // Temporary Solution: Override the close method to do nothing.
+    // The EntityManagerFactory is managed by Spring, so this should not be a problem.
+    //
+    private static class WebformJpaObjectFactory extends JpaObjectFactoryBase{
+        public WebformJpaObjectFactory(
+                EntityManagerFactory emf, 
+                SQLDateTimePatterns sqlDateTimePatterns) {
+            super(emf, sqlDateTimePatterns);
+        }
+        @Override
+        public void close() {
+//                super.close();
+        }
+    }
+    
     @Autowired private EntityManagerFactory entityMangerFactory;
-
+    
     @Override
     public EntityManagerFactory entityManagerFactory() {
         return this.entityMangerFactory;
@@ -24,17 +42,6 @@ public class WebformJpaConfiguration extends JpaConfiguration{
 
     @Override
     @Bean @Scope("singleton") public JpaObjectFactory jpaObjectFactory() {
-//Exception occured when Hibernate calls JpaObjectFactory.close
-//The JpaObjectFactory.close method, closes the EntityManagerFactory
-//Temporary Solution: Override the close method to do nothing
-//The EntityManagerFactory is managed by Spring, so this should not be a problem.
-
-        return new JpaObjectFactoryBase(
-                this.entityManagerFactory(), this.sqlDateTimePatterns()){
-            @Override
-            public void close() {
-//                super.close();
-            }
-        };
+        return new WebformJpaObjectFactory(entityManagerFactory(), sqlDateTimePatterns());
     }
 }
